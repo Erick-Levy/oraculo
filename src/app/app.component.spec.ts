c7982e188c1dac6a1bfbcfc629b44fe899f78ac2
odairviolrodriguespereira <odair.viol@gmail.com>
2023-01-10 21:58:41 -0300

    add git pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'oraculodosenhor'`, () => {
+  it(`should have as title 'oraculo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('oraculodosenhor');
+    expect(app.title).toEqual('oraculo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('oraculodosenhor app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('oraculo app is running!');
   });
 });
